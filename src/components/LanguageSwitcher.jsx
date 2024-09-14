a6dada5a885759c6587f465adc8ae7332e58b23e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-15 00:49:03 +0200

    "Refactor LanguageSwitcher component: change checkbox to separate buttons for each language, update state and event handling logic"

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -3,49 +3,50 @@ import { useTranslation } from "react-i18next";
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
-  const [isChecked, setIsChecked] = useState(i18n.language === "en");
+  const [isChecked, setIsChecked] = useState(false);
 
   useEffect(() => {
     const savedLanguage = localStorage.getItem("language");
     if (savedLanguage) {
       i18n.changeLanguage(savedLanguage);
-      setIsChecked(savedLanguage === "en");
+      setIsChecked(true);
+    } else {
+      i18n.changeLanguage("en");
+      localStorage.setItem("language", "en");
+      setIsChecked(true);
     }
-  }, [i18n]);
+  }, []);
 
-  const handleCheckboxChange = () => {
-    const newLanguage = isChecked ? "de" : "en";
-    i18n.changeLanguage(newLanguage);
-    localStorage.setItem("language", newLanguage);
+  const handleLanguageChange = (language) => {
+    i18n.changeLanguage(language);
+    localStorage.setItem("language", language);
     setIsChecked(!isChecked);
   };
 
-  const languages = ["en", "de"];
-
   return (
     <div>
-      <label className="inline-flex items-center cursor-pointer select-none themeSwitcherThree">
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-          className="sr-only"
-        />
-        <div className="shadow-card flex h-[28px] w-[64px] items-center justify-between rounded-md bg-white p-1">
-          {languages.map((lang) => (
-            <span
-              key={lang}
-              className={`flex h-6 w-6 text-[12px] items-center justify-center rounded ${
-                (isChecked && lang === "en") || (!isChecked && lang === "de")
-                  ? "text-white bg-teal-600 dark:bg-slate-950"
-                  : "text-gray-800"
-              }`}
-            >
-              {lang.toUpperCase()}
-            </span>
-          ))}
-        </div>
-      </label>
+      <div className="shadow-card flex h-[28px] w-[64px] items-center justify-between rounded-md bg-white p-1">
+        <span
+          onClick={() => handleLanguageChange("en")}
+          className={`flex h-6 w-6 text-[12px] items-center justify-center rounded cursor-pointer ${
+            isChecked
+              ? "text-white bg-teal-600 dark:bg-slate-950"
+              : "text-gray-800"
+          }`}
+        >
+          EN
+        </span>
+        <span
+          onClick={() => handleLanguageChange("de")}
+          className={`flex h-6 w-6 text-[12px] items-center justify-center rounded cursor-pointer ${
+            !isChecked
+              ? "text-white bg-teal-600 dark:bg-slate-950"
+              : "text-gray-800"
+          }`}
+        >
+          DE
+        </span>
+      </div>
     </div>
   );
 };
